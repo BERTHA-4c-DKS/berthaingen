a3b71ff0fbedbf4e96a3f15da0794c1f7b159442
Loriano Storchi <loriano@storchi.org>
2021-04-23 17:02:31 +0200

    minimal setup

--- pybgen/pybgen.py
+++ pybgen/pybgen.py
@@ -1,2 +1,22 @@
 import json
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f","--inputfile", help="Specify XYX input file", required=True, \
+        type=str, default="")
+    parser.add_argument("-j","--jsonbasisfile", \
+        help="Specify BERTHA JSON file for fitting and basis (default: fullsets.json)", \
+        required=False, type=str, default="fullsets.json")
+
+    args = parser.parse_args()
+    
+    basisdata = None
+    with open(args.jsonbasisfile) as f:
+        basisdata = json.load(f)
+
+    
+
+    
 
